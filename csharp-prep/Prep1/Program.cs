bd4951f08b5738745541b9cb42cdac21b15efbe7
mistShard <nwa22001@byui.edu>
2023-01-05 15:26:55 +0100

    Added comments to the code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their first name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
-
+        
+        // Ask the user for their last name.
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
         Console.WriteLine();
