655e9e63dd53436cd2c7db179273eb7733da6fc5
mistShard <nwa22001@byui.edu>
2023-01-09 19:37:09 +0100

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int intGuess = 0;
+        int count = 0;
+        string ans = "";
+
+        void main()
+        {
+            string finAns = startGame(intGuess, count, ans);
+
+            if (finAns == "yes")
+            {
+                startGame(intGuess, count, ans);
+            }
+            else
+            {
+                Console.WriteLine("Goodbye. Thank you for playing!");
+            }
+        }
+
+        string startGame(int intGuess, int count, string ans)
+        {   
+            Random rnd = new Random();
+            int num = rnd.Next(2, 101);
+
+            do
+            {   
+            Console.Write("What is your guess? ");
+            string strGuess = Console.ReadLine();
+            intGuess = int.Parse(strGuess);
+
+            if (intGuess < num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (intGuess > num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            count++;
+            
+            } while (intGuess != num);
+
+        Console.WriteLine($"Tries: {count}");
+        Console.WriteLine(""); Console.Write("Input 'yes' if you would like to play again: ");
+        ans = Console.ReadLine();
+
+        return ans;
+        
+        }
+
+        main();
+
     }
 }
